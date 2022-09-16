314a43b46bbf813320a35910200158dcb3bb4643
주동호 <100278301+JOODON@users.noreply.github.com>
2022-09-16 22:06:55 +0900

    SpringMVC

---
+++ src/main/java/config/WebMvcContestConfiguration.java
@@ -0,0 +1,7 @@
+package config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+//MVC를 구현하기전에 컨피그를 상속 받아서 오버라이딩 하는방식으로 사용!
+public class WebMvcContestConfiguration extends WebMvcConfigurerAdapter {
+}
